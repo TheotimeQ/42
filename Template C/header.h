a04f34e0da24feb25ebc92d8c95328530afa7ad4
Zelinsta <theotimeq@gmail.com>
2022-10-03 08:47:31 +0200

    test

---
+++ Template C/header.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelinsta <zelinsta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/25 11:05:42 by tquere            #+#    #+#             */
+/*   Updated: 2022/09/23 09:03:19 by zelinsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+int	ft_atoi(char *str);
+
+// typedef struct s_map_var
+// {
+// 	int		x;
+// 	int		y;
+// 	char	empty;
+// 	char	obstacle;
+// 	char	full;
+// 	char	**map;
+// 	int		sol_x;
+// 	int		sol_y;
+// 	int		sol_side_len;
+// 	int		error;
+// 	int		stop;
+// 	int		file;
+// 	int		visual;
+// 	int		crt_lin;
+// 	int		last_count_size;
+// 	int		malloc_len;
+// }	t_map_var;
+
+#endif
