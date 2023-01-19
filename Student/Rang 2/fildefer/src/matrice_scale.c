f28cebbbbfd0619a9c848e1f403beec2ef2af9e0
Theotime Quere <tquere@z1r3p1.42lyon.fr>
2023-01-19 16:50:00 +0100

    RENDU 2

--- Student/Rang 2/fildefer/src/matrice_scale.c
+++ Student/Rang 2/fildefer/src/matrice_scale.c
@@ -6,7 +6,7 @@
 /*   By: tquere <tquere@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 15:38:31 by tquere            #+#    #+#             */
-/*   Updated: 2022/12/11 12:33:06 by tquere           ###   ########.fr       */
+/*   Updated: 2023/01/13 18:56:15 by tquere           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void	scale(t_fdf *fdf, int x, int y)
 	point_3d_proj = fdf->mat_3d_proj->array[y * fdf->mat_3d_proj->size_x + x];
 	if (point_3d_proj != NULL)
 	{
-		point_3d_proj->x = (((point_3d_proj->x
-						/ point_3d_proj->w) + 1) / 2) * fdf->res_x;
-		point_3d_proj->y = (((point_3d_proj->y
-						/ point_3d_proj->w) + 1) / 2) * fdf->res_y;
+		point_3d_proj->x = (((point_3d_proj->x /
+						point_3d_proj->w) + 1) * 0.5) * fdf->res_x;
+		point_3d_proj->y = (((point_3d_proj->y /
+						point_3d_proj->w) + 1) * 0.5) * fdf->res_y;
 		point_3d_proj->z = point_3d_proj->z / point_3d_proj->w;
 	}
 }
